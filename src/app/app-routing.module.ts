3944c684a8e311e95175411f20067004861568d1
Pedro Costa <costapm96@gmail.com>
2024-02-14 11:25:22 +0000

    fixed notfound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component'; 
 import { AuthGuard } from './auth.guard';
 import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 const routes: Routes = [
   { path: 'pokemons', component: PokemonListComponent, canActivate: [AuthGuard] },
   { path: 'pokemons/:id', component: PokemonDetailComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
